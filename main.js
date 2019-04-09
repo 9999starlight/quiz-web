1cffd001903b6bd783e80ea2ccdbcdcbbb79f701
9999starlight <40492494+9999starlight@users.noreply.github.com>
2019-04-09 09:55:05 +0200

    transfered

---
+++ main.js
@@ -0,0 +1,140 @@
+window.addEventListener('load', intro);
+
+function intro() {
+    // Uvod - sakrij sadržaj kviza, dugme za  pokretanje
+    document.querySelector('.main-con').classList.add('none');
+    document.querySelector('#pokreniKviz').addEventListener('click', kviz);
+}
+
+function kviz() {
+    document.querySelector('.contnaslov').classList.add('none'); // ukloni uvod
+    document.querySelector('.main-con').classList.remove('none');
+    const container = document.querySelector('#container');
+    const tim = document.querySelector('#vreme');
+    let jsonPitanja = [];
+    let trenutnoPitanje = 0; // brojač-pitanja
+    let zbir = 0; // brojač-zbir tačnih odgovora
+
+    fetch('pitanja.json')
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(d => {
+                jsonPitanja.push([d.pitanje, d.tacan, d.netacan1,
+                    d.netacan2, d.netacan3
+                ]);
+            });
+            const pitanja = jsonPitanja.slice(0, 10).map(() => {
+                return jsonPitanja.splice(Math.floor(Math.random() *
+                    jsonPitanja.length), 1)[0];
+            }, jsonPitanja.slice());
+            console.log(pitanja);
+            napraviPitanja();
+
+            function napraviPitanja() {
+                // zaustavi kada završi niz pitanja, rezultat, reset brojača:
+                if (trenutnoPitanje >= pitanja.length) {
+                    kraj();
+                    clearInterval(sInt);
+                    trenutnoPitanje = 0;
+                    zbir = 0;
+                    jsonPitanja = [] // praznim niz za novi fetch data
+                    return false; // zaustavljam funkciju
+                }
+                // Pitanja, opcije, random sort opcija, prikaz
+                document.querySelector('#naslov').innerHTML =
+                    `${trenutnoPitanje + 1} / ${pitanja.length}`;
+                const pitanje = pitanja[trenutnoPitanje][0];
+                const sveOpcije = []
+                sveOpcije.push(pitanja[trenutnoPitanje][1],
+                    pitanja[trenutnoPitanje][2],
+                    pitanja[trenutnoPitanje][3],
+                    pitanja[trenutnoPitanje][4]);
+                sveOpcije.sort(() => Math.random() - 0.5);
+                container.innerHTML = ''; // prazan container za naredni krug
+                let listaOpcija = '';
+                sveOpcije.forEach(op => {
+                    if (op != undefined && op === pitanja[trenutnoPitanje][1]) {
+                    listaOpcija += `<label><input type = 'radio'
+                        name = 'opcije' id = 't'
+                        value = '${op}'><div>${op}</div></label>`
+                    } else if (op != undefined) {
+                    listaOpcija += `<label><input type = 'radio'
+                        name = 'opcije'
+                        value = '${op}'><div>${op}</div></label>`;
+                    } else if (pitanja[trenutnoPitanje][2] === undefined) {
+                    listaOpcija = `<input type = 'text' name = 'opcije'
+                        placeholder="unesite odgovor" autofocus>`
+                    }
+                })
+                container.innerHTML +=
+                    `<div class = 'pitanje borderR'>${pitanje}<div>`;
+                container.innerHTML += listaOpcija;
+                container.innerHTML += `<button>Sledeće pitanje</button>`;
+                document.querySelector('button')
+                    .addEventListener('click', proveriOdgovor);
+            }
+
+            // setovanje vremena za kviz.
+            const odbrojOd = new Date().getTime() + 602000;
+            // odbrojavanje po sekundu, račun za minute and sekunde
+            const sInt = setInterval(() => {
+                    const razlika = odbrojOd - new Date().getTime();
+                let minuti = Math.floor(
+                    (razlika % (1000 * 60 * 60)) / (1000 * 60));
+                    let sekunde = Math.floor((razlika % (1000 * 60)) / 1000);
+                    sekunde < 10 ? (sekunde = `0${sekunde}`) : sekunde;
+                    minuti < 10 ? (minuti = `0${minuti}`) : minuti;
+                    // prikaz odbrojavanja i akcije po isteku vremena
+                razlika >= 0 ? tim.innerHTML = `${minuti} : ${sekunde}` :
+                    (clearInterval(sInt), kraj(),
+                        tim.innerHTML = `Vreme je isteklo!`);
+                },
+                1000,
+                napraviPitanja
+            );
+
+            function proveriOdgovor() {
+                const opcije =
+                    document.querySelectorAll('input[name="opcije"]');
+                const txt = document.querySelector('input[type=text]');
+                opcije.forEach(op => {
+                    let att = op.getAttribute('id');
+                    if (att === 't') {
+                        op.parentNode.classList.add('tacanBorder');
+                    }
+                    if (op.checked) {
+                        if (att === 't') {
+                            zbir++;
+                            op.parentNode.classList.add('tacan');
+                        } else {
+                            op.parentNode.classList.add('netacan');
+                        }
+                    }
+                    if (op = txt) {
+                        if (txt.value.toLowerCase() ===
+                            pitanja[trenutnoPitanje][1]) {
+                            zbir++
+                            op.classList.add('tacanBorder');
+                        } else {
+                            op.classList.add('netacanBorder');
+                        }
+                    }
+                });
+                console.log(zbir);
+                // Naredno pitanje. setTimeout da se prikaže tačnost odgovora
+                trenutnoPitanje++;
+                setTimeout(function () {
+                    napraviPitanja();
+                }, 2000);
+            }
+
+            function kraj() {
+                const procenat =
+                    parseFloat((zbir * 100) / pitanja.length).toFixed(2);
+                container.innerHTML = `<div class = 'pitanje borderR'>
+                Osvojili ste ${zbir} od ${pitanja.length} poena ili
+                ${procenat}%</div>`;
+                document.querySelector('#naslov').innerHTML =`Kviz je završen`;
+            }
+        });
+}
